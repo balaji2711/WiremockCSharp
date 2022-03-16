4d298a57c3318b2501f29cfd434a5c8819c221d8
Z0049RAP <balaji.g@siemens.com>
2022-03-16 12:53:15 +0530

    Initial Commit

---
+++ WiremockProject/ExampleWithStub.cs
@@ -0,0 +1,128 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+
+namespace WiremockProject
+{
+    public class ExampleWithStub
+    {
+        private static WireMockServer stub;
+        private static string baseUrl;
+
+        [OneTimeSetUp]
+        public static void StartServer()
+        {
+            ExtentReportUtility.SetupExtentReport("Regression Test Run", "Automation Testing Report");
+            var port = new Random().Next(5000, 6000);
+            baseUrl = "http://localhost:" + port;
+            stub = WireMockServer.Start(new WireMockServerSettings
+            {
+                Urls = new[] { "http://+:" + port },
+                ReadStaticMappings = true
+            });
+        }
+
+        [OneTimeTearDown]
+        public static void StopServer()
+        {
+            stub.Stop();
+        }
+
+        [SetUp]
+        public void CreateTest()
+        {
+            string name = TestContext.CurrentContext.Test.Name.ToString();
+            ExtentReportUtility.CreateTest(TestContext.CurrentContext.Test.Name.ToString());
+        }
+
+        [TearDown]
+        public void endTest()
+        {
+            try
+            {
+                var testStatus = TestContext.CurrentContext.Result.Outcome;
+                //ExtentReportUtility.TestStatus(testStatus.ToString());
+            }
+            catch (Exception ex)
+            {
+                ExtentReportUtility.SetTestStatusFail(ex.Message.ToString());
+            }
+            finally
+            {
+                ExtentReportUtility.FlushReport();
+            }
+        }
+
+        [Test]
+        public void ValidateWiremock()
+        {
+            try
+            {
+                ExtentReportUtility.AssignCategory("Smoke");
+                var bodyContent = new[]
+                                    { new {id = 1 }, new {id = 2 } };
+                stub.Given(
+                    Request
+                    .Create()
+                        .WithPath("/api/products"))
+                    .RespondWith(
+                        Response.Create()
+                            .WithStatusCode(200)
+                            .WithHeader("Content-Type", "aplication/json")
+                            .WithBodyAsJson(bodyContent));
+
+                var client = new RestClient(baseUrl);
+                var request = new RestRequest("/api/products");
+                var response = client.Execute(request);
+                Assert.AreEqual(200, (int)response.StatusCode);
+                Assert.AreEqual(JsonConvert.SerializeObject(bodyContent), response.Content, "Failed : Response is not matching");
+                ExtentReportUtility.LogReport(AventStack.ExtentReports.Status.Pass, "Response is matching successfully - " + response.Content);
+            }
+            catch (Exception ex)
+            {
+                ExtentReportUtility.SetTestStatusFail(ex.Message.ToString());
+            }
+        }
+
+        [Test]
+        public void ValidateResponse()
+        {
+            try
+            {
+                ExtentReportUtility.AssignCategory("Regression");
+                var bodyContent = new[]
+                                    { new {id = 1 }, new {id = 2 } };
+                stub.Given(
+                    Request
+                    .Create()
+                        .WithPath("/api/products"))
+                    .RespondWith(
+                        Response.Create()
+                            .WithStatusCode(200)
+                            .WithHeader("Content-Type", "aplication/json")
+                            .WithBodyAsJson(bodyContent));
+
+                var client = new RestClient(baseUrl);
+                var request = new RestRequest("/api/products");
+                var response = client.Execute(request);
+                Assert.AreEqual(200, (int)response.StatusCode);
+                Assert.AreEqual(JsonConvert.SerializeObject(bodyContent), response.Content, "Failed : Response is not matching");
+                ExtentReportUtility.LogReport(AventStack.ExtentReports.Status.Pass, "Response is matching successfully - " + response.Content);
+            }
+            catch (Exception ex)
+            {
+                ExtentReportUtility.SetTestStatusFail(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ WiremockProject/ExtentReportUtility.cs
@@ -0,0 +1,112 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiremockProject
+{
+    public class ExtentReportUtility
+    {
+        public static ExtentReports ExtentReports;
+        static string folderName = "WiremockReports";
+        public static ExtentHtmlReporter ExtentHtmlReporter;
+        public static ExtentTest TestCase; //Should not be static for parallel run
+        public static string CurrentRunFolderName = string.Format("{0}-{1}-{2}-{3}-{4}-{5}", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+        public static string filePath = @"C:/" + folderName + "/" + Environment.MachineName + "/" + CurrentRunFolderName;
+
+        public static void SetupExtentReport(string reportName, string docTitle)
+        {
+            //ExtentV3HtmlReporter
+            ExtentHtmlReporter = new ExtentHtmlReporter(filePath + "\\index.html");
+            ExtentHtmlReporter.Config.Theme = Theme.Dark;
+            ExtentHtmlReporter.Config.DocumentTitle = docTitle;
+            ExtentHtmlReporter.Config.ReportName = reportName;
+
+            ExtentReports = new ExtentReports();
+            ExtentReports.AttachReporter(ExtentHtmlReporter);
+            ExtentReports.AddSystemInfo("Machine", Environment.MachineName);
+            ExtentReports.AddSystemInfo("OS", Environment.OSVersion.VersionString);
+            ExtentReports.AddSystemInfo("Author", "Balaji G");
+        }
+
+        public static void CreateTest(string testName)
+        {
+            TestCase = ExtentReports.CreateTest(testName);
+        }
+
+        public static void LogReport(Status status, string message)
+        {
+            TestCase.Log(status, message);
+        }
+
+        public static void TestStatus(string status)
+        {
+            if (status.Equals("Passed"))
+                TestCase.Pass("Test Passed");
+            else if (status.Equals("Failed"))
+                ExtentReportUtility.SetTestStatusFail();
+            else
+                ExtentReportUtility.SetTestStatusSkipped();
+        }
+
+        public static void AssignCategory(string category)
+        {
+            TestCase.AssignCategory(category);
+        }
+
+        public static void SetStepStatusPass(string stepDescription)
+        {
+            TestCase.Log(Status.Pass, stepDescription);
+        }
+
+        public static void SetTestStatusFail(string message = null)
+        {
+            var printMessage = "<p><b>Test FAILED!</b></p>";
+            if (!string.IsNullOrEmpty(message))
+            {
+                printMessage += $"Message: <br>{message}<br>";
+            }
+            //var mediaEntity=ExtentReportUtility.CaptureScreenShot("Image_"+TestLogger.CurrentRunFolderName);
+            TestCase.Fail(printMessage);
+            //TestCase.Fail(printMessage,mediaEntity);
+            //TestCase.AddScreenCaptureFromBase64String(ExtentReportUtility.ScreenCaptureAsBase64String(), "Screenshot on Error:");
+        }
+
+        public static void SetTestStatusSkipped()
+        {
+            TestCase.Skip("Test skipped!");
+        }
+
+        public static void SetStepStatusWarning(string stepDescription)
+        {
+            TestCase.Log(Status.Warning, stepDescription);
+        }
+
+        //public static string ScreenCaptureAsBase64String()
+        //{
+        //    ITakesScreenshot ts = (ITakesScreenshot)Application.driver;
+        //    Screenshot screenshot = ts.GetScreenshot();
+        //    return screenshot.AsBase64EncodedString;
+        //}
+
+        //public static MediaEntityModelProvider CaptureScreenShot(string name)
+        //{
+        //    var screenshot = ((ITakesScreenshot)Application.driver).GetScreenshot().AsBase64EncodedString;
+        //    return MediaEntityBuilder.CreateScreenCaptureFromBase64String(screenshot, name).Build();
+        //}
+
+        public static void FlushReport()
+        {
+            ExtentReports.Flush();
+        }
+
+        public static void AttachScreenshot()
+        {
+            //TestCase.AddScreenCaptureFromBase64String(ExtentReportUtility.ScreenCaptureAsBase64String(), "Screenshot on Error:");
+        }
+    }
+}
